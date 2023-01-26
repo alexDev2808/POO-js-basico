0482f03c2c4f61dd2079c162d790cd8a75d7e03d
AlexDev2808 <colores.2808.8000@gmail.com>
2023-01-26 10:50:00 -0600

    Utilizando la sintaxis de clases para crear prototipos

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ Student.prototype.aprobarCurso = function(nuevoCurso){
 }
 
 const alexis = new Student("Alexis", 22, ["Curso de Kotlin", "Curso de JAVA"])
+
+
+
+// Prototipos con sintaxis de clases
+
+class Student2 {
+    // metodo constructor, se ejecuta al crear una instancia
+    constructor(name, age, cursosAprobados){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    // Metodos
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso)
+    }
+
+
+}
+
+const jesus = new Student2('Jesus', 22, ["Curso de Scope y Closures", "Curso de manipulacion de Arrays"])
